4a5052d1c201a216f9cadd7cd70f70fb2dbe35ab
MiaadEstakhri <miaad8104@gmail.com>
2022-09-26 14:49:59 +0330

    with css pure and javascript vanilla

---
+++ products.js
@@ -0,0 +1,56 @@
+export const productsData = [
+    {
+        id:1,
+        title : "panel 1 bed",
+        price:10.99,
+        imageUrl:"./images/bed1.jpg"
+    },
+    {
+        id:2,
+        title : "panel 2 bed",
+        price:20.99,
+        imageUrl:"./images/bed2.jpg"
+    },
+    {
+        id:3,
+        title : "panel 3 bed",
+        price:30.99,
+        imageUrl:"./images/bed3.jpg"
+    },
+      {
+        id:4,
+        title : "panel 4 bed",
+        price:40.99,
+        imageUrl:"./images/bed4.jpg"
+    },
+    {
+        id:5,
+        title : "panel 5 bed",
+        price:50.99,
+        imageUrl:"./images/bed1.jpg"
+    },
+      {
+        id:6,
+        title : "panel 6 bed",
+        price:60.99,
+        imageUrl:"./images/bed1.jpg"
+    },
+    {
+        id:7,
+        title : "panel 7 bed",
+        price:70.99,
+        imageUrl:"./images/bed1.jpg"
+    },
+      {
+        id:8,
+        title : "panel 8 bed",
+        price:80.99,
+        imageUrl:"./images/bed1.jpg"
+    },
+    {
+        id:9,
+        title : "panel 9 bed",
+        price:90.99,
+        imageUrl:"./images/bed1.jpg"
+    },
+]
